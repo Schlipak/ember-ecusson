b22f8a824532532d817db0861e5db4e00186f6bb
de-mat_b <de-mat_b@epitech.eu>
2017-02-20 19:35:52 +0100

    Updated index.js to import SCSS

--- index.js
+++ index.js
@@ -2,5 +2,10 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-ecusson'
+  name: 'ember-ecusson',
+
+  included: function(app) {
+  	app.import('app/styles/app.scss');
+  	this._super.included.call(this, app);
+  }
 };
