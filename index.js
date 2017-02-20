cdab3956caeb6460dba4ee6bba1e2e47f878a00f
de-mat_b <de-mat_b@epitech.eu>
2017-02-20 20:55:38 +0100

    Updated index.js

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 /* jshint node: true */
 'use strict';
 
-const CONTENT_FOR_HEAD = '<link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700|Merriweather:400,700|Expletus+Sans|Lato:400,700|Fira+Mono" rel="stylesheet"><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">';
+const STYLESHEETS_URIS = [
+  'https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700|Merriweather:400,700|Expletus+Sans|Lato:400,700|Fira+Mono',
+  'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
+];
 
 module.exports = {
   name: 'ember-ecusson',
 
   contentFor: function(type, config) {
     if (type == 'head') {
-      return CONTENT_FOR_HEAD;
+      let links = "";
+      STYLESHEETS_URIS.forEach((uri) => {
+        links += `<link rel="stylesheet" href="${uri}">\n`
+      });
+      return links;
     }
   }
 };
