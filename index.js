dc81463c9c0d08e2d0905d120a27330f7c79cfa6
de-mat_b <de-mat_b@epitech.eu>
2017-02-20 20:07:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 /* jshint node: true */
 'use strict';
 
+const CONTENT_FOR_HEAD = '<link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700|Merriweather:400,700|Expletus+Sans|Lato:400,700|Fira+Mono" rel="stylesheet"><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">';
+
 module.exports = {
-  name: 'ember-ecusson'
+  name: 'ember-ecusson',
+
+  contentFor: function(type, config) {
+    if (type == 'head') {
+      return CONTENT_FOR_HEAD;
+    }
+  }
 };
