a732c40f22409a641c21e013e47fb1636c0e5950
de-mat_b <de-mat_b@epitech.eu>
2017-02-20 19:39:47 +0100

    ADDON PLS

--- index.js
+++ index.js
@@ -2,10 +2,5 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-ecusson',
-
-  included: function(app) {
-  	app.import('app/styles/app.scss');
-  	this._super.included.call(this, app);
-  }
+  name: 'ember-ecusson'
 };
